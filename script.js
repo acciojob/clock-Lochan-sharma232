6f795c079a12a52c0441d3f064bbb22a8f1df9b8
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-22 14:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let timerDisplay=document.getElementById("timer");
+let currentDate=new Date();
+//update the time in every second
 //your JS code here. If required.
+setInterval(
+	function () {
+		//get the current date and time
+		currentDate=new Date();
+        //update the timer display
+		timerDisplay.innerHTML=currentDate.toLocaleString();
+	},1000
+	)
